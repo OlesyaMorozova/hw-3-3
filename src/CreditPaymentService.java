f7c6b55c66b969f97a2f8928d780f66ebe059213
Olesya Morozova <ovmorozova12@yandex.ru>
2024-06-01 10:35:32 +0400

    Changed monthly rate

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(double percent, int term, int sum) {
-        double d = percent / term / 100;
-        double result = sum * (d * Math.pow(d + 1, term) / (Math.pow(d + 1, term) - 1));
+        double monthlyRate = percent / 12 / 100;
+        double result = sum * (monthlyRate * Math.pow(monthlyRate + 1, term) / (Math.pow(monthlyRate + 1, term) - 1));
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         System.out.println();
-        System.out.println("на 12 месяцев:");
+        System.out.println("Ежемесячный платеж на 12 месяцев:");
         System.out.println((int) service.calculate(9.99, 12, 1_000_000));
 
         System.out.println();
-        System.out.println("на 24 месяца:");
+        System.out.println("Ежемесячный платеж на 24 месяца:");
         System.out.println((int) service.calculate(9.99, 24, 1_000_000));
 
         System.out.println();
-        System.out.println("на 36 месяцев:");
+        System.out.println("Ежемесячный платеж на 36 месяцев:");
         System.out.println((int) service.calculate(9.99, 36, 1_000_000));
     }
 }
